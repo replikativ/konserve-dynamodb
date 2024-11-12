866e83e946a95b4b78d3c69fa9cd54c5b0da8b21
Christian Weilbach <ch_weil@topiq.es>
2024-11-12 15:26:59 -0800

    Fix format.

--- src/konserve_dynamodb/core.clj
+++ src/konserve_dynamodb/core.clj
@@ -1,52 +1,50 @@
 (ns konserve-dynamodb.core
   "DynamoDB based konserve backend."
   (:require
-    [konserve.impl.defaults :refer [connect-default-store]]
-    [konserve.impl.storage-layout :refer [PBackingStore PBackingBlob PBackingLock -delete-store]]
-    [konserve.utils :refer [async+sync *default-sync-translation*]]
-    [superv.async :refer [go-try-]]
-    [taoensso.timbre :refer [info trace]])
+   [konserve.impl.defaults :refer [connect-default-store]]
+   [konserve.impl.storage-layout :refer [PBackingStore PBackingBlob PBackingLock -delete-store]]
+   [konserve.utils :refer [async+sync *default-sync-translation*]]
+   [superv.async :refer [go-try-]]
+   [taoensso.timbre :refer [info trace]])
   (:import
-    (java.io
-      ByteArrayInputStream)
-    (java.net
-      URI)
-    (java.util
-      HashMap
-      Map)
-    (software.amazon.awssdk.auth.credentials
-      AwsBasicCredentials
-      StaticCredentialsProvider)
-    (software.amazon.awssdk.core
-      SdkBytes)
-    (software.amazon.awssdk.services.dynamodb
-      DynamoDbClient)
-    (software.amazon.awssdk.services.dynamodb.model
-      AttributeDefinition
-      AttributeDefinition$Builder
-      AttributeValue
-      CreateTableRequest
-      CreateTableRequest$Builder
-      DeleteItemRequest
-      DeleteItemResponse
-      DeleteTableRequest
-      DescribeTableRequest
-      GetItemRequest
-      KeySchemaElement
-      KeySchemaElement$Builder
-      KeyType
-      ProvisionedThroughput
-      ProvisionedThroughput$Builder
-      PutItemRequest
-      PutItemResponse
-      ResourceNotFoundException
-      ScalarAttributeType
-      ScanRequest
-      ScanResponse)))
-
+   (java.io
+    ByteArrayInputStream)
+   (java.net
+    URI)
+   (java.util
+    HashMap
+    Map)
+   (software.amazon.awssdk.auth.credentials
+    AwsBasicCredentials
+    StaticCredentialsProvider)
+   (software.amazon.awssdk.core
+    SdkBytes)
+   (software.amazon.awssdk.services.dynamodb
+    DynamoDbClient)
+   (software.amazon.awssdk.services.dynamodb.model
+    AttributeDefinition
+    AttributeDefinition$Builder
+    AttributeValue
+    CreateTableRequest
+    CreateTableRequest$Builder
+    DeleteItemRequest
+    DeleteItemResponse
+    DeleteTableRequest
+    DescribeTableRequest
+    GetItemRequest
+    KeySchemaElement
+    KeySchemaElement$Builder
+    KeyType
+    ProvisionedThroughput
+    ProvisionedThroughput$Builder
+    PutItemRequest
+    PutItemResponse
+    ResourceNotFoundException
+    ScalarAttributeType
+    ScanRequest
+    ScanResponse)))
 
 (set! *warn-on-reflection* true)
-
 
 (defn dynamodb-client
   "Creates a new DynamoDB client using the provided options, with explicit credential handling."
@@ -58,10 +56,9 @@
       (.region builder (software.amazon.awssdk.regions.Region/of (:region opts))))
     (when (:access-key opts)
       (let [credentials (StaticCredentialsProvider/create
-                          (AwsBasicCredentials/create (:access-key opts) (:secret opts)))]
+                         (AwsBasicCredentials/create (:access-key opts) (:secret opts)))]
         (.credentialsProvider builder credentials)))
     (.build builder)))
-
 
 (defn create-dynamodb-table
   [^DynamoDbClient client table-name {:keys [read-capacity write-capacity]}]
@@ -105,7 +102,6 @@
         (.build create-table-request-builder)]
     (.createTable client create-table-request)))
 
-
 (defn table-exists?
   [^DynamoDbClient client ^String table-name]
   (try
@@ -117,14 +113,12 @@
     (catch ResourceNotFoundException _
       false)))
 
-
 (defn delete-dynamodb-table
   [^DynamoDbClient client ^String table-name]
   (let [^DeleteTableRequest request (-> (DeleteTableRequest/builder)
                                         (.tableName table-name)
                                         .build)]
     (.deleteTable client request)))
-
 
 (defn ^PutItemResponse put-item
   [^DynamoDbClient client ^String table-name ^HashMap item]
@@ -133,7 +127,6 @@
                                     (.item item)
                                     .build)]
     (.putItem client request)))
-
 
 (defn ^Map get-item
   [^DynamoDbClient client ^String table-name ^HashMap key]
@@ -146,7 +139,6 @@
     (catch Exception _
       nil)))
 
-
 (defn ^DeleteItemResponse delete-item
   [^DynamoDbClient client ^String table-name ^HashMap key]
   (let [^DeleteItemRequest request (-> (DeleteItemRequest/builder)
@@ -155,7 +147,6 @@
                                        .build)]
     (.deleteItem client request)))
 
-
 (defn ^ScanResponse scan-table
   [^DynamoDbClient client ^String table-name]
   (let [^ScanRequest request (-> (ScanRequest/builder)
@@ -163,19 +154,16 @@
                                  .build)]
     (.scan client request)))
 
-
 (defn- attribute-value-s
   [^String s]
   (.build (.s (AttributeValue/builder) s)))
-
 
 (defn- attribute-value-b
   [^bytes b]
   (.build (.b (AttributeValue/builder) (SdkBytes/fromByteArray b))))
 
-
 (defrecord DynamoDBBlob
-  [table ^String key data ^clojure.lang.Atom fetched-object]
+           [table ^String key data ^clojure.lang.Atom fetched-object]
 
   PBackingBlob
 
@@ -194,97 +182,85 @@
                                              {:data @data})))
                            (reset! data {})))))
 
-
   (-close
     [_ env]
     (if (:sync? env) nil (go-try- nil)))
-
 
   (-get-lock
     [_ env]
     (if (:sync? env) true (go-try- true)))
 
-
   (-read-header
     [_ env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try-
-                  (when-not @fetched-object
-                    (reset! fetched-object (get-item (:client table) (:table table) (hash-map "Key" (attribute-value-s key)))))
-                  (let [^Map fetched-obj @fetched-object
-                        ^AttributeValue attr-value (.get fetched-obj "Header")
-                        ^SdkBytes sdk-bytes (.b attr-value)
-                        ^bytes byte-array (.asByteArray sdk-bytes)]
-                    byte-array))))
-
+                 (when-not @fetched-object
+                   (reset! fetched-object (get-item (:client table) (:table table) (hash-map "Key" (attribute-value-s key)))))
+                 (let [^Map fetched-obj @fetched-object
+                       ^AttributeValue attr-value (.get fetched-obj "Header")
+                       ^SdkBytes sdk-bytes (.b attr-value)
+                       ^bytes byte-array (.asByteArray sdk-bytes)]
+                   byte-array))))
 
   (-read-meta
     [_ _meta-size env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try-
-                  (let [^Map fetched-obj @fetched-object
-                        ^AttributeValue attr-value (.get fetched-obj "Meta")
-                        ^SdkBytes sdk-bytes (.b attr-value)
-                        ^bytes byte-array (.asByteArray sdk-bytes)]
-                    byte-array))))
-
+                 (let [^Map fetched-obj @fetched-object
+                       ^AttributeValue attr-value (.get fetched-obj "Meta")
+                       ^SdkBytes sdk-bytes (.b attr-value)
+                       ^bytes byte-array (.asByteArray sdk-bytes)]
+                   byte-array))))
 
   (-read-value
     [_ _meta-size env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try-
-                  (let [^Map fetched-obj @fetched-object
-                        ^AttributeValue attr-value (.get fetched-obj "Value")
-                        ^SdkBytes sdk-bytes (.b attr-value)
-                        ^bytes byte-array (.asByteArray sdk-bytes)]
-                    byte-array))))
-
+                 (let [^Map fetched-obj @fetched-object
+                       ^AttributeValue attr-value (.get fetched-obj "Value")
+                       ^SdkBytes sdk-bytes (.b attr-value)
+                       ^bytes byte-array (.asByteArray sdk-bytes)]
+                   byte-array))))
 
   (-read-binary
     [_ _meta-size locked-cb env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try-
-                  (let [^Map fetched-obj @fetched-object
-                        ^AttributeValue attr-value (.get fetched-obj "Value")
-                        ^SdkBytes sdk-bytes (.b attr-value)
-                        ^bytes obj (.asByteArray sdk-bytes)]
-                    (locked-cb {:input-stream
-                                (ByteArrayInputStream. obj)
-                                :size (alength obj)})))))
-
+                 (let [^Map fetched-obj @fetched-object
+                       ^AttributeValue attr-value (.get fetched-obj "Value")
+                       ^SdkBytes sdk-bytes (.b attr-value)
+                       ^bytes obj (.asByteArray sdk-bytes)]
+                   (locked-cb {:input-stream
+                               (ByteArrayInputStream. obj)
+                               :size (alength obj)})))))
 
   (-write-header
     [_ header env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try- (swap! data assoc :header header))))
 
-
   (-write-meta
     [_ meta env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try- (swap! data assoc :meta meta))))
-
 
   (-write-value
     [_ value _meta-size env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try- (swap! data assoc :value value))))
 
-
   (-write-binary
     [_ _meta-size blob env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try- (swap! data assoc :value blob)))))
-
 
 (extend-protocol PBackingLock
   Boolean
   (-release [_ env]
     (if (:sync? env) nil (go-try- nil))))
 
-
 (defrecord DynamoDBStore
-  [^DynamoDbClient client ^String table]
+           [^DynamoDbClient client ^String table]
 
   PBackingStore
 
@@ -293,81 +269,70 @@
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try- (DynamoDBBlob. this store-key (atom {}) (atom nil)))))
 
-
   (-delete-blob
     [_ store-key env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try- (delete-item client table (hash-map "Key" (attribute-value-s store-key))))))
-
 
   (-blob-exists?
     [_ store-key env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try- (boolean (seq (get-item client table (hash-map "Key" (attribute-value-s store-key))))))))
 
-
   (-copy
     [_ from to env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try-
-                  (let [item (get-item client table (hash-map "Key" (attribute-value-s from)))]
-                    (when item
-                      (put-item client table (assoc item "Key" (attribute-value-s to))))))))
-
+                 (let [item (get-item client table (hash-map "Key" (attribute-value-s from)))]
+                   (when item
+                     (put-item client table (assoc item "Key" (attribute-value-s to))))))))
 
   (-atomic-move
     [_ from to env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try-
-                  (let [item (get-item client table (hash-map "Key" (attribute-value-s from)))]
-                    (when item
-                      (put-item client table (assoc item "Key" (attribute-value-s to)))
-                      (delete-item client table (hash-map "Key" (attribute-value-s from))))))))
-
+                 (let [item (get-item client table (hash-map "Key" (attribute-value-s from)))]
+                   (when item
+                     (put-item client table (assoc item "Key" (attribute-value-s to)))
+                     (delete-item client table (hash-map "Key" (attribute-value-s from))))))))
 
   (-create-store
     [_ env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try- (info "DynamoDB table setup complete."))))
 
-
   (-sync-store
     [_ env]
     (if (:sync? env) nil (go-try- nil)))
-
 
   (-delete-store
     [_ env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try-
-                  (try
-                    (delete-dynamodb-table client table)
-                    (info "DynamoDB store deleted.")
-                    (catch ResourceNotFoundException _
-                      (info "DynamoDB table does not exist."))))))
-
+                 (try
+                   (delete-dynamodb-table client table)
+                   (info "DynamoDB store deleted.")
+                   (catch ResourceNotFoundException _
+                     (info "DynamoDB table does not exist."))))))
 
   (-migratable
     [_ _key _store-key env]
     (if (:sync? env) nil (go-try- nil)))
 
-
   (-migrate
     [_ _migration-key _key-vec _serializer _read-handlers _write-handlers env]
     (if (:sync? env) nil (go-try- nil)))
-
 
   (-keys
     [_ env]
     (async+sync (:sync? env) *default-sync-translation*
                 (go-try-
-                  (let [^ScanResponse response (scan-table client table)]
-                    (->> (.items response)
-                         (map (fn [^Map item]
-                                (let [^AttributeValue attr-value (.get item "Key")]
-                                  (.s attr-value))))
-                         doall))))))
-
+                 (let [^ScanResponse response (scan-table client table)]
+                   (->> (.items response)
+                        (map (fn [^Map item]
+                               (let [^AttributeValue attr-value (.get item "Key")]
+                                 (.s attr-value))))
+                        doall))))))
 
 (defn connect-store
   [dynamodb-spec & {:keys [opts]
@@ -387,23 +352,20 @@
                       (dissoc params :opts :config))]
     (connect-default-store backing config)))
 
-
 (defn delete-store
   [dynamodb-spec & {:keys [opts]}]
   (let [complete-opts (merge {:sync? true} opts)
         backing (DynamoDBStore. (dynamodb-client dynamodb-spec) (:table dynamodb-spec))]
     (-delete-store backing complete-opts)))
 
-
 (defn release
   "Release the store connection."
   [store env]
   (async+sync (:sync? env) *default-sync-translation*
               (go-try-
-                (let [^DynamoDBStore backing (:backing store)
-                      ^DynamoDbClient client (:client backing)]
-                  (.close client)))))
-
+               (let [^DynamoDBStore backing (:backing store)
+                     ^DynamoDbClient client (:client backing)]
+                 (.close client)))))
 
 (comment
   ;; Testing and usage example:
--- test/konserve_dynamodb/core_test.clj
+++ test/konserve_dynamodb/core_test.clj
@@ -1,17 +1,15 @@
 (ns konserve-dynamodb.core-test
   (:require
-    [clojure.core.async :refer [<!!]]
-    [clojure.test :refer [deftest testing]]
-    [konserve-dynamodb.core :refer [connect-store release delete-store]]
-    [konserve.compliance-test :refer [compliance-test]]))
-
+   [clojure.core.async :refer [<!!]]
+   [clojure.test :refer [deftest testing]]
+   [konserve-dynamodb.core :refer [connect-store release delete-store]]
+   [konserve.compliance-test :refer [compliance-test]]))
 
 (def dynamodb-spec
   {:region "us-west-2"
    :table "konserve-dynamodb-test"
    :access-key (System/getenv "AWS_ACCESS_KEY_ID")
    :secret (System/getenv "AWS_SECRET_ACCESS_KEY")})
-
 
 (deftest dynamodb-compliance-sync-test
   (let [dynamodb-spec (assoc dynamodb-spec :table "konserve-dynamodb-sync-test")
@@ -23,7 +21,6 @@
       (compliance-test store))
     (release store {:sync? true})
     (delete-store dynamodb-spec :opts {:sync? true})))
-
 
 (deftest dynamodb-compliance-async-test
   (let [dynamodb-spec (assoc dynamodb-spec :table "konserve-dynamodb-async-test")
